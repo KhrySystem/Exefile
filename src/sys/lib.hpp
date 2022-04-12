fdabed6b5a803ddc36587f0b172696b17c82d53d
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2022-04-11 20:21:32 -0400

    Ignoring more directories.

--- src/sys/lib.hpp
+++ src/sys/lib.hpp
@@ -1,16 +1,16 @@
 #ifndef LIB_HPP
-#define LIB_HPP
-
-#ifndef GLFW_INCUDE_VULKAN
-    #define GLFW_INCLUDE_VULKAN
-#endif
-#include <GLFW/glfw3.h>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <map>
-#include <stdexcept>
-#define GLM_SWIZZLE_XYZW
-#include <glm.hpp>
-
+    #define LIB_HPP
+    #ifndef GLFW_INCUDE_VULKAN
+        #define GLFW_INCLUDE_VULKAN
+    #endif
+    #include <GLFW/glfw3.h>
+    #include <string>
+    #include <vector>
+    #include <iostream>
+    #include <map>
+    #include <stdexcept>
+    #ifndef GLM_SWIZZLE_XYZW
+        #define GLM_SWIZZLE_XYZW
+    #endif
+    #include <glm.hpp>
 #endif
