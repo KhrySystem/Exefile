fb12a9c510b463c4d3d5d917f25fcc94f5a216f7
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2022-05-04 07:28:59 -0400

    ValidatiolLayers

--- src/dragon/src/dgVulkanInstance.hpp
+++ src/dragon/src/dgVulkanInstance.hpp
@@ -8,15 +8,36 @@ DGAPI namespace Dragon {
 		public:
 			dgVulkanInstance();
 
+			bool checkValidationLayerSupport();
+			std::vector<const char*> getRequiredExtensions();
+			void setupDebugMessenger();
+			VkResult createDebugMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, 
+				const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+
+			VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, 
+				const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger); 
+
+			static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+				VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+
+			void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
 			~dgVulkanInstance();
-			
+
+			void destroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+
 		private:
 			VkApplicationInfo appInfo{};
 			VkInstanceCreateInfo createInfo{};
 			uint32_t glfwExtensionCount, extensionCount;
 			const char** glfwExtensions;
 			std::vector<VkExtensionProperties> extensions;
+			std::vector<const char*> extensionNames;
+			std::vector<const char*> validationLayers = {
+				"VK_LAYER_KHRONOS_validation"
+			};
 			VkInstance instance;
+			VkDebugUtilsMessengerEXT debugMessenger;
 	};
 };
 
